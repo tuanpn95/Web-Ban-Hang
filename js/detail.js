ddadbec939cbd670c9b2df6c3913f15c95a424d9
tuanpn95 <34708764+tuanpn95@users.noreply.github.com>
2020-10-09 11:34:10 +0700

    fix store

--- js/detail.js
+++ js/detail.js
@@ -42,9 +42,9 @@ function viewMoreSelling(){
 
 function viewStore(){
   var x = document.getElementById("showstore");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
